8353ecee04ca75df0105bb86a58129b81b9455a6
Behario <ezetoanton@gmail.com>
2019-11-17 16:51:25 +0300

    Генерация случайной буквы в диапазоне

--- Lesson_1/4.py
+++ Lesson_1/4.py
@@ -21,7 +21,13 @@ RIGHT_FLOAT = float(input("Конец диапазона "))
 NUMB_FLOAT = random() * (RIGHT_FLOAT-LEFT_FLOAT)+LEFT_FLOAT
 print(f"Результат генерации случайного вещественного числа: {round(NUMB_FLOAT, 3)}")
 
-
+ALPHABET = 'abcdefghijklmnopqrstuvwxyz'
+LEFT_LETTER = input("Введите букву английского алфавита - левая граница: ")
+RIGHT_LETTER = input("Введите букву английского алфавита - правая граница: ")
+INDEX_LEFT = ALPHABET.index(LEFT_LETTER)
+INDEX_RIGHT = ALPHABET.index(RIGHT_LETTER)
+INDEX_RANDOM = int(random()*(INDEX_RIGHT-INDEX_LEFT+1)+INDEX_LEFT)
+print(f"Результат генерации случайной буквы: {ALPHABET[INDEX_RANDOM]}")
 
 
 
