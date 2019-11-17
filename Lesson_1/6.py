210319c69de71439c4b146256ed402baaadafd9c
Behario <ezetoanton@gmail.com>
2019-11-17 19:35:27 +0300

    реализация задачи 6

--- Lesson_1/6.py
+++ Lesson_1/6.py
@@ -1,1 +1,8 @@
 # 6.	Пользователь вводит номер буквы в алфавите. Определить, какая это буква.
+
+import string
+
+ALPHABET = string.ascii_lowercase
+LETTER_INDEX = int(input("Введите номер буквы из английского алфавита: "))
+LETTER_FINDER = ALPHABET[LETTER_INDEX-1]
+print(f"Ваша буква: {LETTER_FINDER}")
