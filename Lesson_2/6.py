6ba005718f0d9fa0a0c1222c4d22f2fb8260efa3
Behario <ezetoanton@gmail.com>
2019-11-21 21:17:08 +0300

    реализация 6-й задачи

--- Lesson_2/6.py
+++ Lesson_2/6.py
@@ -5,3 +5,26 @@
 число, чем то, что загадано. Если за 10 попыток число не отгадано,
 то вывести загаданное число.
 """
+from random import random
+
+RANDOM = int(random()*(100 - 0 + 1) + 0)
+n = 0
+
+while n <= 9:
+    ANSWER = int(input("Загадайте число: "))
+    if n < 9:
+        if RANDOM == ANSWER:
+            print(f"Вы угадали, правильное число - {ANSWER}")
+            break
+        elif RANDOM != ANSWER:
+            if RANDOM > ANSWER:
+                print("Ответ неправильный, загаданное вами число меньше, попробуйте еще раз")
+            elif RANDOM < ANSWER:
+                print("Ответ неправильный, загаданное вами число больше, попробуйте еще раз")
+    elif n == 9:
+        if RANDOM == ANSWER:
+            print(f"Вы угадали, правильное число - {ANSWER}")
+        else:
+            print(f"Вы проиграли, правильное число: {RANDOM}")
+
+    n = n + 1
