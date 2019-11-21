bde8e67e8a396ba9a54f0d6abc040f3612ab152d
Behario <ezetoanton@gmail.com>
2019-11-21 23:27:01 +0300

    Реализация 8-й задачи

--- Lesson_2/8.py
+++ Lesson_2/8.py
@@ -3,3 +3,18 @@
  последовательности чисел. Количество вводимых чисел и цифра,
  которую необходимо посчитать, задаются вводом с клавиатуры.
 """
+
+def finder(numbers, number, number_counter = 0):
+    if numbers == 0:
+        return f"{number_counter}"
+    else:
+        if numbers % 10 == number:
+            number_counter += 1
+        numbers = numbers // 10
+        return finder(numbers, number, number_counter)
+
+
+NUMBERS = int(input("Введите составное число: "))
+NUMBER = int(input("Какую цифру ищем: "))
+
+print(f'Мы ищем количество вхождений {NUMBER} в {NUMBERS}, давайте узнаем: ровно {finder(NUMBERS, NUMBER)} раза')
