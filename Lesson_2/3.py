049c5e3b2c07b08378de7145dd85401de75797dd
Behario <ezetoanton@gmail.com>
2019-11-21 20:07:28 +0300

    Какие-то проблемы с гитом

--- Lesson_2/3.py
+++ Lesson_2/3.py
@@ -3,3 +3,15 @@
 цифр и вывести на экран. Например, если введено число 3486,
  то надо вывести число 6843.
 """
+
+NUMBER = int(input("Введите натуральное число: "))
+NUMBER_BACKWARD = 0
+
+while NUMBER > 0:
+    DIG = NUMBER % 10
+    NUMBER = NUMBER // 10
+    NUMBER_BACKWARD = NUMBER_BACKWARD * 10
+    NUMBER_BACKWARD = NUMBER_BACKWARD + DIG
+
+print(NUMBER_BACKWARD)
+
