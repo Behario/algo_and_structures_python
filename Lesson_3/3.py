90101552272787e239528346b67a6a4e69fc0ad3
Behario <ezetoanton@gmail.com>
2019-11-22 17:52:50 +0300

    реализация 3-й задачи

--- Lesson_3/3.py
+++ Lesson_3/3.py
@@ -1,2 +1,21 @@
 #3.	В массиве случайных целых чисел поменять местами минимальный
 # и максимальный элементы.
+
+from random import random
+MAX = 9
+MIN = 0
+LIST_RANDOM = []
+
+for i in range(MIN, MAX + 1):
+    RANDOM = int(random() * (MAX - MIN + 1) + MIN)
+    LIST_RANDOM.append(RANDOM)
+
+print(LIST_RANDOM)
+
+MINIMUM = min(LIST_RANDOM)
+MAXIMUM = max(LIST_RANDOM)
+MIN_INDEX = LIST_RANDOM.index(MINIMUM)
+MAX_INDEX = LIST_RANDOM.index(MAXIMUM)
+LIST_RANDOM[MAX_INDEX], LIST_RANDOM[MIN_INDEX] = LIST_RANDOM[MIN_INDEX], LIST_RANDOM[MAX_INDEX]
+
+print(LIST_RANDOM)
