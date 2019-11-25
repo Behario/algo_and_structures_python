059aadce2c5418e28845312951a89cbc196cf79d
Behario <ezetoanton@gmail.com>
2019-11-25 20:50:57 +0300

    реализация 8-й задачи

--- Lesson_3/8.py
+++ Lesson_3/8.py
@@ -4,3 +4,19 @@
 записывать ее в последнюю ячейку строки.
 В конце следует вывести полученную матрицу.
 """
+LINE = 5
+COLUMN = 4
+matrix = []
+for i in range(LINE):
+    matrix_line = []
+    sum = 0
+    print(f"{i}-я строка:")
+    for j in range(COLUMN - 1):
+        element = int(input())
+        sum += element
+        matrix_line.append(element)
+    matrix_line.append(sum)
+    matrix.append(matrix_line)
+
+for i in matrix:
+    print(i)
